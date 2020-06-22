78d8342375a8c4db3e7e352ff192de0ea4e3acc7
Winningtopas <jay07@live.nl>
2020-06-22 17:28:16 +0200

    Done

--- app/src/main/java/com/example/level5task1/MainActivity.kt
+++ app/src/main/java/com/example/level5task1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.level5task1
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -7,11 +8,10 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.level5task1.MainActivityViewModel
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,21 +22,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         initViews()
         initViewModel()
     }
 
     private fun initViews() {
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra(EditActivity.EXTRA_NOTE, mainActivityViewModel.note.value)
+            startActivity(intent)
         }
     }
-
 
     private fun initViewModel() {
         mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
@@ -48,22 +44,5 @@ class MainActivity : AppCompatActivity() {
                 tvNote.text = note.text
             }
         })
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
--- app/src/main/java/com/example/level5task1/MainActivityViewModel.kt
+++ app/src/main/java/com/example/level5task1/MainActivityViewModel.kt
@@ -2,6 +2,7 @@ package com.example.level5task1
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.level5task1.NoteRepository
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
 
